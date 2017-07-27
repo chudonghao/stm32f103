04bb4a04b96c73fa648f19042a804fefe99517d5
Donghao Chu <799017701@qq.com>
2017-07-27 18:08:30 +0800

    0.0.1
    得到 QVGA YUV422 数据

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <FreeRTOS.h>
 #include <task.h>
 #include <stm32f10x_conf.h>
+
 #define SYSTEM_SUPPORT_OS 1
 extern "C" {
 #include "delay.h"
@@ -17,6 +18,7 @@ extern "C" {
 extern u8 ov_sta;    //在exit.c里 面定义
 const static int camera_w = 240;
 const static int camera_h = 320;
+
 //更新LCD显示
 void camera_refresh(void) {
     u32 j;
@@ -25,7 +27,7 @@ void camera_refresh(void) {
     {
 
         LCD_Scan_Dir(D2U_R2L);        //从上到下,从左到右
-        LCD_Set_Window(0,0,camera_h,camera_w);
+        LCD_Set_Window(0, 0, camera_h, camera_w);
 //        if (lcddev.id == 0X1963)
 //            LCD_Set_Window((lcddev.width - camera_w) / 2, (lcddev.height - camera_h) / 2, camera_w, camera_h);//将显示区域设置到屏幕中央
 //        else if (lcddev.id == 0X5510 || lcddev.id == 0X5310)
@@ -68,46 +70,46 @@ void camera_refresh(void) {
         }
 
 //        printf("\r\n");
-        //rgb图像处理
-        for(int x = 0;x < camera_h;++x){
-            for(int y = 0;y<camera_w;++y){
-                color = LCD_ReadPoint(x,y);
-                u16 tmp;
-                //红点
-                tmp = color & ((u16)0x1f << 11);
-                tmp >>= 11;
-                if(tmp > 28){
-                    POINT_COLOR = RED;
-                }else if (tmp < 10){
-                    POINT_COLOR = BLACK;
-                }else{
-                    POINT_COLOR = WHITE;
-                }
-                LCD_DrawPoint(x,y+240);
-
-                tmp = color & ((u16)0x3f << 5);
-                tmp >>= 5;
-                if(tmp > 50){
-                    POINT_COLOR = GREEN;
-                }else if (tmp < 20){
-                    POINT_COLOR = BLACK;
-                }else{
-                    POINT_COLOR = WHITE;
-                }
-                LCD_DrawPoint(x+320,y);
-
-                tmp = color & ((u16)0x1f);
-                if(tmp > 28){
-                    POINT_COLOR = BLUE;
-                }else if (tmp < 10){
-                    POINT_COLOR = BLACK;
-                }else{
-                    POINT_COLOR = WHITE;
-                }
-                LCD_DrawPoint(x+320,y+240);
-
-            }
-        }
+//        //rgb图像处理
+//        for (int x = 0; x < camera_h; ++x) {
+//            for (int y = 0; y < camera_w; ++y) {
+//                color = LCD_ReadPoint(x, y);
+//                u16 tmp;
+//                //红点
+//                tmp = color & ((u16) 0x1f << 11);
+//                tmp >>= 11;
+//                if (tmp > 28) {
+//                    POINT_COLOR = RED;
+//                } else if (tmp < 10) {
+//                    POINT_COLOR = BLACK;
+//                } else {
+//                    POINT_COLOR = WHITE;
+//                }
+//                LCD_DrawPoint(x, y + 240);
+//
+//                tmp = color & ((u16) 0x3f << 5);
+//                tmp >>= 5;
+//                if (tmp > 50) {
+//                    POINT_COLOR = GREEN;
+//                } else if (tmp < 20) {
+//                    POINT_COLOR = BLACK;
+//                } else {
+//                    POINT_COLOR = WHITE;
+//                }
+//                LCD_DrawPoint(x + 320, y);
+//
+//                tmp = color & ((u16) 0x1f);
+//                if (tmp > 28) {
+//                    POINT_COLOR = BLUE;
+//                } else if (tmp < 10) {
+//                    POINT_COLOR = BLACK;
+//                } else {
+//                    POINT_COLOR = WHITE;
+//                }
+//                LCD_DrawPoint(x + 320, y + 240);
+//
+//            }
+//        }
 
         ov_sta = 0;                    //清零帧中断标记
         LCD_Scan_Dir(DFT_SCAN_DIR);    //恢复默认扫描方向
@@ -148,6 +150,22 @@ int main(void) {
     EXTI8_Init();                                   //使能定时器捕获
 //    OV7670_Window_Set(12, 176, 240, 320);              //设置窗口
     //SCCB_WR_Reg(0x12,0x24);
+
+    //设置为QVGA YUV
+    //参考 https://www.amobbs.com/thread-5490146-1-1.html
+    //{ 0x12, 0x10 }
+    //{ 0x3a, 0x14 }使用固定UV输出
+    //{ 0x3d, 0x80 }使用固定UV输出
+    //{ 0x67, 0x11 }固定U值，0x11，方便测试
+    //{ 0x68, 0xFF }固定V值，0xFF，方便测试
+    //{ 0x40, 0xC0 }
+    SCCB_WR_Reg(0x12, 0x10);
+    //SCCB_WR_Reg(0x3a, 0x14);
+    //SCCB_WR_Reg(0x3d, 0x80);
+    //SCCB_WR_Reg(0x67, 0x11);
+    //SCCB_WR_Reg(0x68, 0xFF);
+    SCCB_WR_Reg(0x40, 0xC0);
+
     OV7670_Window_Set(12, 176, camera_w, camera_h);              //设置窗口
     OV7670_CS = 0;
     LCD_Clear(WHITE);
@@ -159,6 +177,6 @@ int main(void) {
     LCD_Clear(RED);
     delay_ms(100);
     LCD_Clear(BLACK);
-    xTaskCreate(main_task,0,100,0,1,0);
+    xTaskCreate(main_task, 0, 100, 0, 1, 0);
     xPortStartScheduler();
 }
